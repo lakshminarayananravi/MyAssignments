0b852a71b31a8438d98547392729c5327e6cc6e5
lakshminarayananravi <54950406+lakshminarayananravi@users.noreply.github.com>
2025-03-17 19:57:44 +0530

    Add files via upload
    day1 classroom

---
+++ day1/day1/LearnDatatypes.java
@@ -0,0 +1,48 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+		
+		       //byte
+				byte age=30;
+				System.out.println(age);
+				
+				//short
+				short ageOfPerson=128;
+				System.out.println(ageOfPerson);
+				
+				//int
+				int depositAmount=50000;
+				System.out.println(depositAmount);
+				
+				//long
+				long mobileNumber=8925411170l;
+				System.out.println(mobileNumber);
+				
+				//float
+				float rateOfInterest=7.5F;
+				System.out.println(rateOfInterest);
+				
+				float rupee=0.005755595512345F;
+				System.out.println(rupee);
+				
+				double dollar=0.005755595512345;
+				System.out.println(dollar);
+				
+				//char
+				char logoOfTestleaf='T';
+				System.out.println(logoOfTestleaf);
+				
+				//boolean
+				boolean haveBreakfast=false;
+				System.out.println(haveBreakfast);
+				
+				//String
+				String name="Vineeth";
+				System.out.println(name+" "+age);
+
+	}
+
+}
